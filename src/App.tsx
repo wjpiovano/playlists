c6d3803424e1043d68c36073a043eb71be18200a
piuvans <wjpiovano@hotmail.com>
2019-06-13 23:56:17 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { render } from 'react-dom';
+
+function Aggregate() {
+  return (
+    <div style={{ display: 'inline-block', width: '20%' }}>
+      <h2>Some More Stuff</h2>
+    </div>
+  );
+}
+
+function Filter() {
+  return (
+    <div>
+      <img />
+      <input type="text" />
+    </div>
+  );
+}
+
+function Playlist() {
+  return (
+    <div style={{ width: '25%', display: 'inline-block' }}>
+      <img />
+      <h3>Playlist Name</h3>
+      <ul><li>Song1</li><li>Song2</li><li>Song3</li></ul>
+    </div>
+  );
+}
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Super Title</h1>
+      <Aggregate />
+      <Aggregate />
+      <Filter />
+      <div className="playlist-container">
+        <Playlist />
+        <Playlist />
+        <Playlist />
+        <Playlist />
+        <Playlist />
+        <Playlist />
+      </div>
+    </div>
+  );
+}
+
+export default App;
